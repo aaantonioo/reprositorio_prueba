267ed250ced33bed20bfc3136a57cf06bb2910a1
aaantonioo <danielmolina43211@gmail.com>
2024-03-18 16:58:01 +0100

    Hola Mundo

---
+++ UT2_EJ3_Stoian_Antonio.py
@@ -0,0 +1,20 @@
+
+# Antonio Daniel Stoian
+
+# Lista de números
+numeros = [10, 5, 8, 7, 9]
+
+
+suma = 0
+
+for numero in numeros:
+    suma += numero
+promedio = suma / numero
+
+# Imprimir el resultado. 
+
+# Veremos que esta mal las variable ,tendremos que cambiar numero por promedio para que imprima el promedio de la lista 
+# y no imprima la lista entera
+
+print("El promedio es: ", promedio)
+
